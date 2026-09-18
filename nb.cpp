236ce68a40bf95c0924362383233bfecc0b2055e
MKRUSSELLE-e <ngamaleumonkamrussellevire@gmail.com>
2026-09-18 19:15:53 +0100

    declaration du nombre

--- nb.cpp
+++ nb.cpp
@@ -10,3 +10,12 @@ int main() {
    std::cout<<"un nombre aleatoire :"<<nombresecret;
    return 0;
    }
+   int main(){
+
+    int x,a,b;
+    std::cout<<"donner un nombre entre 1 et 100 :";
+    std::cin>>x;
+    std::cout<<"le resultat de l'addition est :"<<x+a;
+    std::cout<<"le resultat de la soustraction est :"<<x-b;
+    return 0;
+   }
